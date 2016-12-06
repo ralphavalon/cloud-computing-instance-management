0bd22f2e3e4d250c0d822764ac9f75446cdb6844
Ralph Avalon <amoedo.raphael@gmail.com>
2016-12-05 23:04:31 -0200

    Starting project

---
+++ src/main/java/com/computing/cloud/CloudComputingApplication.java
@@ -0,0 +1,12 @@
+package com.computing.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudComputingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudComputingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/computing/cloud/CloudComputingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.computing.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudComputingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
