d38d73bd1ed6fc9264a9ab78564d5e9f69f257e4
Ralph Avalon <amoedo.raphael@gmail.com>
2016-12-09 22:04:05 -0200

    Adding User JPA Repository

--- src/main/java/com/computing/cloud/dao/UserRepository.java
+++ src/main/java/com/computing/cloud/dao/UserRepository.java
@@ -1,11 +1,11 @@
 package com.computing.cloud.dao;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.computing.cloud.domain.User;
 
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long>{
 	
 	User findByUsernameAndPassword(String username, String password);
 	
-	User save(User user);
-
 }
