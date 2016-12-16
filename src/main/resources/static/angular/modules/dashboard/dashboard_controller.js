f71a6e97fec3ba239c9458f0d6df154ccdc65c4b
raphael.amoedo <raphael.amoedo@infobase.com.br>
2016-12-16 17:48:26 -0200

    Create New Instance button

--- src/main/resources/static/angular/modules/dashboard/dashboard_controller.js
+++ src/main/resources/static/angular/modules/dashboard/dashboard_controller.js
@@ -1,20 +1,32 @@
-angular.module('dashboard', []).controller("DashboardCtrl", function($scope, $state) {
+angular.module('dashboard', []).controller("DashboardCtrl",
+		
+		function($scope, $state, $filter) {
 
-    $scope.instances = [{
-    	"id":1,
-    	"instanceId":1,
-    	"instanceName": "small",
-    	"userInstanceName":"u-a-b-1",
-    	"operatingSystem":"Windows Server 2003",
-    	"status":"ON"
-    }, 
-    {
-    	"id":1,
-    	"instanceId":1,
-    	"instanceName": "small",
-    	"userInstanceName":"u-a-b-1",
-    	"operatingSystem":"Windows Server 2003",
-    	"status":"ON"
-    }];
-    
-});
+			$scope.instances = [ {
+				"id" : 1,
+				"instanceId" : 1,
+				"instanceName" : "small",
+				"userInstanceName" : "u-a-b-1",
+				"operatingSystem" : "Windows Server 2003",
+				"status" : "ON"
+			}, {
+				"id" : 1,
+				"instanceId" : 1,
+				"instanceName" : "small",
+				"userInstanceName" : "u-a-b-1",
+				"operatingSystem" : "Windows Server 2003",
+				"status" : "OFF"
+			} ];
+
+			$scope.statusIs = function(status) {
+				return function(instance) {
+					console.log('ITEM: ' + status);
+					console.log('instance: ' + instance);
+					return instance.status === status;
+				};
+			};
+			
+			$scope.on = $scope.instances.filter($scope.statusIs('ON'));
+			$scope.off = $scope.instances.filter($scope.statusIs('OFF'));
+
+		});
